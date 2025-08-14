a4675c50bbf3eaf3dd6a9423411507f36ce0857a
vinnybrunn00 <vinibruno.dev@gmail.com>
2025-08-13 21:27:32 -0300

    commit

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -2,9 +2,21 @@ import 'package:process_run/process_run.dart';
 
 class Services {
   final Shell _shell = Shell(options: ShellOptions());
-  final String _args = 'pm2 restart 0';
+  final String _path = 'C:\\Users\\vindev\\Desktop\\projects\\tester\\main.js';
 
   Future<void> execPowershellCommand() async {
-    await _shell.run(_args);
+    await _shell.runExecutableArguments('powershell', [
+      '-Command',
+      'Start-Process',
+      '-FilePath',
+      'powershell.exe',
+      '-ArgumentList',
+      '"-Command pm2 start '
+          '${_path.replaceAll('\\', '/')}'
+          '"',
+      '-WindowStyle',
+      'Hidden',
+      '-Wait',
+    ]);
   }
 }
